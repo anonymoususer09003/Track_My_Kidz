695ca38d87311c2909947e0e98dfce7bb75fe0b0
soyma20 <slaviksoyma2014@gmail.com>
2024-03-19 20:21:13 +0200

    fix: add await for deviceId

--- src/Services/LoginServices/Login.ts
+++ src/Services/LoginServices/Login.ts
@@ -1,12 +1,12 @@
-import api from "@/Services";
-import { getDeviceId, getUniqueId } from "react-native-device-info";
+import api from '@/Services';
+import { getUniqueId } from 'react-native-device-info';
+
 export default async (loginObject: any, type: string) => {
-  let data =
-    type != "parent"
-      ? { ...loginObject, deviceId: getUniqueId() }
-      : { ...loginObject, deviceId: "" };
+  const deviceId = await getUniqueId();
+  const data =
+    type != 'parent'
+      ? { ...loginObject, deviceId }
+      : { ...loginObject, deviceId: '' };
 
-  const res = await api.post(`/auth/login/${type}`, data);
-
-  return res;
+  return await api.post(`/auth/login/${type}`, data);
 };
