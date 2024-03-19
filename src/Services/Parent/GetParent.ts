98067354d0869dd235b28c7ef6d47425fc544067
soyma20 <slaviksoyma2014@gmail.com>
2024-03-19 14:33:45 +0200

    add protection from redundant api call

--- src/Services/Parent/GetParent.ts
+++ src/Services/Parent/GetParent.ts
@@ -6,11 +6,13 @@ import {
   storeStudentParentDetail,
 } from "@/Storage/MainAppStorage";
 export default async (id: number) => {
-  console.log("logss0s909s090s90");
+  console.log("GetParent.ts line 9");
   const getInfo = await getStudentParentDetail();
   if (getInfo) {
     return JSON.parse(getInfo);
   } else {
+    if (!id || isNaN(id)) return
+
     const response = await api.get(`/user/parent/${id}`);
     await storeStudentParentDetail(JSON.stringify(response.data));
     return response.data;
