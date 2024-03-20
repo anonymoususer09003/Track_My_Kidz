035c9833afdf452e2cd864235037611a321f19de
soyma20 <slaviksoyma2014@gmail.com>
2024-03-20 20:17:42 +0200

    refactor: remove livestream

--- src/Navigators/Main/MainNavigator.tsx
+++ src/Navigators/Main/MainNavigator.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import LeftDrawerNavigator from "@/Navigators/Main/LeftDrawerNavigator";
-import { createStackNavigator } from "@react-navigation/stack";
-import { useSelector } from "react-redux";
-import { LivestreamState } from "@/Store/Livestream";
-import { WelcomeMessageModal } from "@/Modals";
-import TwoFACodeModal from "../../Modals/TwoFACodeModal";
-import LogicComponent from "@/Components/LogicComponent";
-import { ModalState } from "@/Store/Modal";
+import React from 'react';
+import LeftDrawerNavigator from '@/Navigators/Main/LeftDrawerNavigator';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useSelector } from 'react-redux';
+import { WelcomeMessageModal } from '@/Modals';
+import TwoFACodeModal from '../../Modals/TwoFACodeModal';
+import LogicComponent from '@/Components/LogicComponent';
+import { ModalState } from '@/Store/Modal';
 
 const DrawerScreen = ({ navigation }: { navigation: any }) => {
   const welcomeMessageModal = useSelector(
-    (state: { modal: ModalState }) => state.modal.welcomeMessageModal
+    (state: { modal: ModalState }) => state.modal.welcomeMessageModal,
   );
+  console.log('MainNavigator.tsx line 14 welcomeMessageModal', welcomeMessageModal);
 
   return (
     <>
@@ -25,14 +25,9 @@ const DrawerScreen = ({ navigation }: { navigation: any }) => {
 // @refresh reset
 const MainNavigator = () => {
   const Stack = createStackNavigator();
-  const livestream = useSelector(
-    (state: { livestream: LivestreamState }) => state.livestream
-  );
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      {!livestream.livestreamOpen && (
-        <Stack.Screen name="Drawer" component={DrawerScreen} />
-      )}
+      <Stack.Screen name="Drawer" component={DrawerScreen} />
     </Stack.Navigator>
   );
 };
